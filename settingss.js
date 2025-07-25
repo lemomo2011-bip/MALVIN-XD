46514954d243afcb3126c5e552f41ec25c6f3635
lemomo2011-bip <lemomo2011@gmail.com>
2025-07-25 15:02:11 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~Mm81VIzI#YaK7ufeijXpS0x638UJRbpb6wsnkxd_-eElxXtXaRgk",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "24162413308", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
